da3adf87eb9d6912abe0bab262df664bd74fc1e5
kerem <kerem@phonoclick.com>
2021-08-29 14:57:39 +0300

    init consturctor

--- UdemyXUnitTest/UdemyXUnitTest.Test/CalculatorTest.cs
+++ UdemyXUnitTest/UdemyXUnitTest.Test/CalculatorTest.cs
@@ -6,16 +6,22 @@ namespace UdemyXUnitTest.Test
 {
   public class CalculatorTest
   {
+    private Calculator _calculator;
+
+    public CalculatorTest()
+    {
+      _calculator = new Calculator();
+    }
+
     [Fact]
     public void AddTest()
     {
       //arrange
       int a = 5;
       int b = 20;
-      var calculator = new Calculator();
 
       //act
-      var total = calculator.Add(a, b);
+      var total = _calculator.Add(a, b);
 
       //assert
       Assert.Equal(25, total);
@@ -45,11 +51,8 @@ namespace UdemyXUnitTest.Test
     [InlineData(9, 8, 17)]
     public void AddTest2(int a, int b, int expectedTotal)
     {
-      //arrange
-      var calculator = new Calculator();
-
       //act
-      var actTotal = calculator.Add(a, b);
+      var actTotal = _calculator.Add(a, b);
 
       //assert
       Assert.Equal(expectedTotal, actTotal);
