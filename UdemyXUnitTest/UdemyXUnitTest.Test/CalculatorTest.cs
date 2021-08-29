8a7d30a7b99234d63b1ae8736529842517d1f64e
kerem <kerem@phonoclick.com>
2021-08-29 14:12:34 +0300

    first test

---
+++ UdemyXUnitTest/UdemyXUnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,23 @@
+﻿using UdemyXUnitTest.App;
+using Xunit;
+
+namespace UdemyXUnitTest.Test
+{
+  public class CalculatorTest
+  {
+    [Fact]
+    public void AddTest()
+    {
+      //arrange
+      int a = 5;
+      int b = 20;
+      var calculator = new Calculator();
+
+      //act
+      var total = calculator.Add(a, b);
+
+      //assert
+      Assert.Equal(25, total);
+    }
+  }
+}
